e6d09651968c375799cfac401eb3614c5e072ade
Luca Mosetti <luka.mosetti@gmail.com>
2023-07-14 11:04:18 +0200

    add `toString()`

--- src/main/java/it/unitn/utils/Range.java
+++ src/main/java/it/unitn/utils/Range.java
@@ -1,3 +1,10 @@
 package it.unitn.utils;
 
-public record Range(int gt, int lte) {}
+public record Range(int gt, int lte) {
+
+    @Override
+    public String toString() {
+        return "(%d, %d]".formatted(gt, lte);
+    }
+
+}
